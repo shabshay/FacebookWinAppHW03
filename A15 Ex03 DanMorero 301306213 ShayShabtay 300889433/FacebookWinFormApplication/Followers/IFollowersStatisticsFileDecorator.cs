f5b639233b3dccc96b92e9697c8d95cc6d94d7af
shabshay <shay.shabshay@gmail.com>
2015-01-20 00:22:46 +0200

    2

---
+++ A15 Ex03 DanMorero 301306213 ShayShabtay 300889433/FacebookWinFormApplication/Followers/IFollowersStatisticsFileDecorator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacebookWinFormApplication.Followers
+{
+    public interface IFollowersStatisticsFileDecorator : IFollowersStatisticsFileBuilder
+    {
+        IFollowersStatisticsFileBuilder FileBuilder { get; set; }
+    }
+}
